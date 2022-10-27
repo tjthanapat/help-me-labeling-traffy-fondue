24d8e8b216f2883538a63737f9dc8d2dc6a9c987
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2022-10-27 19:20:12 +0700

    Add firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAlmu2FfEU06co1gBAmZhlMv4_q8y28mTU',
+  authDomain: 'help-me-labeling.firebaseapp.com',
+  projectId: 'help-me-labeling',
+  storageBucket: 'help-me-labeling.appspot.com',
+  messagingSenderId: '783187226370',
+  appId: '1:783187226370:web:30668c9b0153c6cf201c1b',
+};
+
+export { firebaseConfig };
