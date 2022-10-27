a7090bacf18cbd210d9e16fcb2f0a3e52bfe363d
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2022-10-27 16:11:20 +0700

    Add LandingPage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AssessPage from './containers/AssessPage';
+import LandingPage from './containers/LandingPage';
 import SuccessPage from './containers/SuccessPage';
 
 const App = () => {
   const [ticket, setTicket] = useState(null);
+  const [loadingTicket, setLoadingTicket] = useState(true);
   const [success, setSuccess] = useState(false);
+  const [showAssessPage, setShowAssessPage] = useState(false);
+
+  useEffect(() => {
+    setLoadingTicket(false);
+  }, []);
+  
   if (success) {
     return <SuccessPage />;
-  } else {
+  } else if (showAssessPage) {
     return (
       <>
-        <AssessPage
-          ticket={ticket}
-          setSuccess={setSuccess}
-        />
+        <AssessPage ticket={ticket} setSuccess={setSuccess} />
       </>
+    );
+  } else {
+    return (
+      <LandingPage
+        loadingTicket={loadingTicket}
+        setShowAssessPage={setShowAssessPage}
+      />
     );
   }
 };
